94af0c2509d00427d4d45b6f4e371c8f7f02abed
Pablo Martinez <pablo.rodriguezmar@gmail.com>
2023-01-07 18:33:54 +0100

    structure example of multimodule project

---
+++ module1/src/main/java/com/paru/tests/App.java
@@ -0,0 +1,13 @@
+package com.paru.tests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
